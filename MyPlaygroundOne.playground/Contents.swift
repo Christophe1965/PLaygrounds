aff1f05bf3aa8b960289a927086beb2e0d904a67
Gebruiker <delap007@planet.nl>
2017-01-18 10:03:35 +0100

    Initial commit

---
+++ MyPlaygroundOne.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+str+="!"
+print (str)
+var str2 = "goodbye"
+print (str2)
+var number = 101
+print (number)
+var str3 = "very good"
+print (str3)
+var str4 = "the weather is great today"
+print (str4)
+var str5 = "amsterdam"
+print (str5)
+
+//:define a variable of type string, explicitly
+var str6: String = "amsterdam"
+print(str+str5)
+
+//:define a variable of type interger, explicitly
+var number1: Int = 102
+print (number1)
+
+
+
+
+
+
+
+
---
+++ MyPlaygroundTwo.playground/Contents.swift
@@ -0,0 +1,142 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+let numberOfTrees: Int = 1000
+var forest: Int
+forest = 1000
+var deadTrees: Int = 50
+let forestName: String = "Sherwood"
+let forestDescription =
+"\(forestName) has a forest of \(forest) and \(numberOfTrees) of trees and \(deadTrees) dead trees."
+print (forestDescription)
+var str1 = "Nottingham"
+let cityName: String = "Nottingham"
+var numberOfPeople: Int = 320000
+numberOfPeople = 320000
+let cityDescription = "\(cityName) has a population of \(numberOfPeople) people."
+var str2 = "Saint-Brieuc"
+let cityNameTwo: String = "Saint-Brieuc"
+var numberOfPeople2: Int = 70000
+numberOfPeople2 = 70000
+let cityDescription2 = "\(cityName) has a population of \(numberOfPeople) people compare to \(cityNameTwo) with a population of \(numberOfPeople2) people."
+print (cityDescription)
+print (cityDescription2)
+var str3 = "England"
+var str4 = "Bretagne"
+let countryName: String = "England"
+let countryName1: String = "Bretagne"
+
+var counter = 20
+counter = counter + 2
+
+var counter2 = 10
+counter2 = counter2 - 2
+
+var result = counter + counter2
+var result2 = counter * counter2
+var result3 = counter / counter2
+var result4 = counter % counter2
+
+print (result)
+print (result2)
+print (result3)
+print (result4)
+
+var counter3 = (5 * 3)  / (2 * 2)
+let a = 5
+let b = 10
+
+var average = (a * b) / 2
+print (average)
+
+var c: Double =  3.5
+var d = 20
+
+var result5 = c + Double(d)
+
+
+let temperatureFarenheit: Double = 120
+var result6 = (temperatureFarenheit * 1.8) + 32
+
+var count =  0
+count = count + 2
+
+var decrement = 20
+decrement = decrement - 2
+
+print("Hello World")
+
+var cityhall = "citycenter"
+print (cityhall)
+
+let amsterdam = "Masterdam"
+
+print("hello world \(cityhall)")
+
+
+var numberOfPeople3: Int = 800000 //this is an explicite definition of an interger
+var numberOfPeople4  = 20000 //this is an implicite definition of an interger
+
+var age = 16
+
+age = 16
+print (age)
+
+age = 30
+print (age)
+
+age = 60
+print (age)
+
+var raining:Bool = false
+var sunny:Bool = false
+if raining {
+    print("raining")
+} else if sunny { print("clear")
+} else {print ("I dunno")}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
