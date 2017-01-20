7cc26f95fed31aa0373ce7a80ac2f254f08181ce
Gebruiker <delap007@planet.nl>
2017-01-20 17:54:39 +0100

    Conditional and Function chapter 12

---
+++ ConditionalRainingSunny.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var raining:Bool = false
+var sunny:Bool = false
+if raining {
+    print("raining")
+} else if sunny {
+    print("clear")
+} else {
+    print ("I dunno")
+}
---
+++ Functions chapter 12.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// defining a function / >>>Functions execute code.<<< Swift provide function and other function are the one i create myself.
+
+func printgreeting () {
+    print("Hello, playground.")
+}
+printgreeting()
+
+// Function Parameters
+
+
