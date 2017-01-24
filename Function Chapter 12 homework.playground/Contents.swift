2e1e030612a76a3339c37be27a31f524b7d29483
Gebruiker <delap007@planet.nl>
2017-01-24 12:35:01 +0100

    New functions homework

---
+++ Function Chapter 12 homework.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//func printGreeting() {
+//    print("Hello, The Donald")
+//}
+//printGreeting()
+//
+//func printPersonalGreeting(name: String) {
+//    print("Hello\(name), welcome to your playground.")
+//}
+//printPersonalGreeting(name: "The donald")
+
+//func printPersonalGreeting(name: String) {
+//    print("Hello \(name), welcome to your playground.")
+//}
+//printPersonalGreeting(name: "The Donald")
+//
+//func divisionDescriptionFor (numerator: Double, denominator: Double) {
+//    print("\(numerator) divided by \(denominator) equals \(numerator / denominator)")
+//}
+//divisionDescriptionFor (numerator: 9.0, denominator: 3.0)
+
+
+func printPersonalGreeting(to name: String) {
+     print("Hello \(name), welcome to your playground.")
+}
+printPersonalGreeting(to: "The Donald")
+
+func divisionDescriptionFor (numerator: Double, denominator: Double) {
+    print("\(numerator) divided by \(denominator) equals \(numerator / denominator)")
+    
+}
+divisionDescriptionFor(numerator: 9.0, denominator: 3.0)
+
+
+
