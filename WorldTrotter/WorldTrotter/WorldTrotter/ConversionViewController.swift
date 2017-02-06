931dac95d09e4d07d6a2a4fe680e6dc22e153bc7
Gebruiker <delap007@planet.nl>
2017-02-06 18:26:09 +0100

    World Trotter 06022017

---
+++ WorldTrotter/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Christophe Delaporte on 06/02/2017.
+//  Copyright © 2017 SeriousNerdFarm. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+   
+// MARK: properties
+    @IBOutlet var celsiusLabel: UILabel!
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusValue()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+            
+        } else {
+            return nil
+        }
+    }
+    @IBOutlet var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCelsiusValue()
+    }
+    
+// MARK: custom functions
+    func updateCelsiusValue() {
+        if let celsiusValue = celsiusValue     {
+//            celsiusLabel.text = "\(celsiusValue)"
+              celsiusLabel.text =
+                numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+//        print("current text: \(textField.text)")
+//        print("replacement text: \(string)")
+//        
+//        return true
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        
+        if existingTextHasDecimalSeparator != nil,
+        replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+    
+    }
+    
+        
+
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+        
+    }()
+    
+// MARK: these are the button actions
+        
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+//        celsiusLabel.text = textField.text 
+        
+//        if let text = textField.text, !text.isEmpty {
+//            celsiusLabel.text = text
+//        } else {
+//            celsiusLabel.text = "???"
+//        }
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+            
+        } else {
+            fahrenheitValue = nil
+        }
+        
+    }
+    
+    @IBAction func dismissKeyboard(_sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+}
